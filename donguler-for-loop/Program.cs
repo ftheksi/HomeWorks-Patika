e8f21be0b8941a811cd68b07ebe3bfd9b80a1e0b
ftheksi <fatiheksi.59@gmail.com>
2023-08-09 14:56:29 +0300

    .

---
+++ donguler-for-loop/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+Console.Write("Lütfen Bir sayı giriniz:");
+int sayac =int.Parse(Console.ReadLine());
+
+for (int i = 1; i <=sayac; i++)
+{
+    if(i%2==1)
+    Console.WriteLine(i);
+}
+
+
+int tekToplam =0;
+int ciftToplam =0;
+for (int i = 1; i <=1000; i++)
+{
+    if(i%2==1)
+        tekToplam+=i;
+    else
+        ciftToplam+=i;
+}
+Console.WriteLine("tekToplam:"+tekToplam);
+Console.WriteLine("ciftToplam:"+ciftToplam);
+
+
+
+//break-continue
+
+for (int i = 1; i <10; i++){
+    
+    if(i==4)
+        break;
+    Console.WriteLine(i);
+}
+for (int i = 1; i <10; i++){
+    
+    if(i==4)
+        continue;
+    Console.WriteLine(i);
+}
