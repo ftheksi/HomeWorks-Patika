070ae4cf5d0d0360a4378f91ce321052dd78fc28
ftheksi <fatiheksi.59@gmail.com>
2023-08-14 12:03:43 +0300

    .

---
+++ Diziler/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string[] renkler = new string[5];
+
+string[] hayvanlar = {"Kedi", "Köpek", "Kuş", "Maymun"};
+
+int[] dizi;
+dizi = new int[5];
+
+renkler[0] = "Mavi";
+dizi[3] =10;
+Console.WriteLine(hayvanlar[1]);
+Console.WriteLine(dizi[3]);
+Console.WriteLine(renkler[0]);
+
+// Döngülerle dizi kullanımı
+
+Console.WriteLine("Lütfen dizinin eleman sayısını giriniz: ");
+int diziUzunlugu = int.Parse(Console.ReadLine());
+int[] ornekDizi = new int [diziUzunlugu];
+
+for (int i = 0; i < diziUzunlugu; i++)
+{
+    Console.WriteLine("Lütfen {0}. sayı giriniz: ", i+1);
+    ornekDizi[i]= int.Parse(Console.ReadLine());
+}
+
+int toplam = 0;
+foreach (var sayi in ornekDizi)
+    toplam+= sayi;
+
+Console.WriteLine("ortalama:", + toplam/diziUzunlugu);
+
